eadb79c9e1a7b77566e92bd2f514cf2711d13061
Alex Machado <amachado@oxfordclub.com>
2025-01-24 16:22:08 -0500

    add rest of template tests

--- src/__tests__/iterableClient.test.ts
+++ src/__tests__/iterableClient.test.ts
@@ -11,6 +11,7 @@ describe('IterableClient', () => {
     mock.reset()
   })
 
+  // test for getTemplates()
   it('should call the /templates endpoint on getTemplates', async () => {
     const mockResponse = {
       templates: [
@@ -19,6 +20,22 @@ describe('IterableClient', () => {
           createdAt: 1234567890123,
           updatedAt: 1234567890123,
           name: 'Template name',
+          creatorUserId: 'someuser@test.com',
+          messageTypeId: 123456,
+        },
+        {
+          templateId: 12345678,
+          createdAt: 1234567890123,
+          updatedAt: 1234567890123,
+          name: 'Template name 2',
+          creatorUserId: 'someuser@test.com',
+          messageTypeId: 123456,
+        },
+        {
+          templateId: 12345678,
+          createdAt: 1234567890123,
+          updatedAt: 1234567890123,
+          name: 'Template name 3',
           creatorUserId: 'someuser@test.com',
           messageTypeId: 123456,
         },
@@ -34,5 +51,82 @@ describe('IterableClient', () => {
     expect(mock.history.get.length).toBe(1) // ensures get request was made
     expect(mock.history.get[0].url).toBe('/templates') // checks the url
     expect(mock.history.get[0].headers?.['Api-Key']).toBe(apiKey) // checks the headers
+  })
+
+  // test for getTemplateById()
+  it('should call the /templates/${templateType}/get?templateId=${templateId} endpoint on getTemplateById', async () => {
+    const mockResponse = {
+      templateId: 12345678,
+      metadata: {
+        templateId: 12345678,
+        createdAt: 1234567890123,
+        updatedAt: 1234567890123,
+        name: 'Template Name',
+        creatorUserId: 'user@example.com',
+        messageTypeId: 123456,
+        clientTemplateId: 'yothisisatemplateid6',
+      },
+      name: 'Template Name',
+      fromName: 'My Email From Name',
+      fromEmail: 'fromemail@example.com',
+      subject: 'Email subject line',
+      preheaderText: '',
+      ccEmails: [],
+      bccEmails: [],
+      html: '<p>This is a great template! {{unsubscribeUrl}}</p>',
+      plainText: '',
+      dataFeedIds: [],
+      cacheDataFeed: true,
+      mergeDataFeedContext: false,
+      clientTemplateId: 'yothisisatemplateid6',
+      messageTypeId: 123456,
+      creatorUserId: 'user@example.com',
+    }
+    const templateType = 'email'
+    const templateId = 12345678
+
+    mock
+      .onGet(
+        `https://api.iterable.com/api/templates/${templateType}/get?templateId=${templateId}`
+      )
+      .reply(200, mockResponse)
+
+    const response = await client.getTemplateById(templateType, templateId)
+    expect(response).toEqual(mockResponse)
+    expect(mock.history.get.length).toBe(1)
+    expect(mock.history.get[0].url).toBe(
+      `/templates/${templateType}/get?templateId=${templateId}`
+    )
+    expect(mock.history.get[0].headers?.['Api-Key']).toBe(apiKey)
+  })
+
+  // test for createTemplate()
+  it('should call the /templates/email/upsert endpoint on createTemplate', async () => {
+    const mockResponse = {
+      msg: 'Upserted 1 templates with IDs: 12345678',
+      code: 'Success',
+      params: null,
+    }
+    const mockData = {
+      name: 'Cool Template Example',
+      fromEmail: 'email@mb.example.com',
+      fromName: 'Email From Name',
+      subject: 'Email Name and Date',
+      html: '<p>This is a great template! {{unsubscribeUrl}}</p>',
+      clientTemplateId: 'yothisisatemplateid',
+      messageTypeId: 123456,
+      creatorUserId: 'test@example.com',
+    }
+
+    mock
+      .onPost('https://api.iterable.com/api/templates/email/upsert', mockData)
+      .reply(200, mockResponse)
+
+    const response = await client.createTemplate(mockData)
+    expect(response).toEqual(mockResponse)
+    expect(mock.history.post.length).toBe(1)
+    expect(mock.history.post[0].url).toBe('/templates/email/upsert')
+    expect(JSON.parse(mock.history.post[0].data)).toEqual(mockData) // Check the request body
+    expect(mock.history.post[0].headers?.['Api-Key']).toBe(apiKey)
   })
 })
