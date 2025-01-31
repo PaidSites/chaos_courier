a745e69af8b1a99793ea24f01481e31db8b0af54
Alex Machado <amachado@oxfordclub.com>
2025-01-31 14:02:00 -0500

    refactor return types

--- src/iterableClient.ts
+++ src/iterableClient.ts
@@ -1,14 +1,10 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
 import {
-  iterableCampaignMedataResponse,
-  iterableCampaignMetricsResponse,
+  Campaign,
   iterableCreateCampaignBody,
   iterableCreateTemplateResponse,
-  iterableGetListsResponse,
-  iterableListCountResponse,
-  iterableMessageTypeResponse,
-  iterableTemplateResponse,
-  iterableTemplatesResponse,
+  List,
+  MessageType,
   Template,
 } from './iterableInterfaces'
 
@@ -28,7 +24,6 @@ export class IterableClient {
   }
   // TODO:
   // - implement rxjs observable
-  // - add types to function calls
 
   /* ==== TEMPLATE CALLS ==== */
   async getTemplates(medium?: string): Promise<Template[]> {
@@ -41,8 +36,8 @@ export class IterableClient {
   async getTemplateById(
     templateType: string,
     templateId: number
-  ): Promise<iterableTemplateResponse> {
-    const response = await this.client.get(
+  ): Promise<Template> {
+    const response: AxiosResponse<Template> = await this.client.get(
       `/templates/${templateType}/get?templateId=${templateId}`
     )
     return response.data
@@ -51,44 +46,52 @@ export class IterableClient {
   async createTemplate(
     data: Record<string, any>
   ): Promise<iterableCreateTemplateResponse> {
-    const response = await this.client.post('/templates/email/upsert', data)
+    const response: AxiosResponse<iterableCreateTemplateResponse> =
+      await this.client.post('/templates/email/upsert', data)
     return response.data
   }
 
   /* ==== MESSAGETYPES CALLS ==== */
-  async getMessageTypes(): Promise<iterableMessageTypeResponse> {
-    const response = await this.client.get('/messageTypes')
-    return response.data
+  async getMessageTypes(): Promise<MessageType[]> {
+    const response: AxiosResponse<{ messageTypes: MessageType[] }> =
+      await this.client.get('/messageTypes')
+    return response.data.messageTypes
   }
 
   /* ==== CAMPAIGN CALLS ==== */
-  async getCampaignsMetadata(): Promise<iterableCampaignMedataResponse> {
-    const response = await this.client.get('/campaigns')
-    return response.data
+  async getCampaignsMetadata(): Promise<Campaign[]> {
+    const response: AxiosResponse<{ campaigns: Campaign[] }> =
+      await this.client.get('/campaigns')
+    return response.data.campaigns
   }
 
-  async getCampaignMetrics(
-    campaignId: number
-  ): Promise<iterableCampaignMetricsResponse> {
-    const response = await this.client.get(
+  async getCampaignMetrics(campaignId: number): Promise<string> {
+    const response: AxiosResponse<string> = await this.client.get(
       `/campaigns/metrics?campaignId=${campaignId}`
     )
     return response.data
   }
 
-  async createCampaign(data: iterableCreateCampaignBody) {
-    const response = await this.client.post('/campaigns/create', data)
+  async createCampaign(data: iterableCreateCampaignBody): Promise<number> {
+    const response: AxiosResponse<number> = await this.client.post(
+      '/campaigns/create',
+      data
+    )
     return response.data
   }
 
   /* ==== LIST CALLS ==== */
-  async getLists(): Promise<iterableGetListsResponse> {
-    const response = await this.client.get('/lists')
-    return response.data
+  async getLists(): Promise<List[]> {
+    const response: AxiosResponse<{ lists: List[] }> = await this.client.get(
+      '/lists'
+    )
+    return response.data.lists
   }
 
-  async getListUserCount(listId: number): Promise<iterableListCountResponse> {
-    const response = await this.client.get(`/lists/${listId}/size`)
+  async getListUserCount(listId: number): Promise<number> {
+    const response: AxiosResponse<number> = await this.client.get(
+      `/lists/${listId}/size`
+    )
     return response.data
   }
 }
